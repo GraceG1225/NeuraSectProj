54e7328a99116f225d64ae79de68770e8ee367e2
HyKNH <lachynguyen70@gmail.com>
2025-09-30 20:06:20 -0700

    view tables

---
+++ neurasect/src/app/datasets/[id]/page.tsx
@@ -0,0 +1,58 @@
+import { supabase } from '../../lib/supabaseClient';
+
+interface Props {
+    params: { id: string };
+}
+
+export default async function DatasetPage({ params }: Props) {
+    const { id } = params;
+
+    const { data, error } = await supabase
+        .from('datasets')
+        .select('*')
+        .eq('id', id)
+        .single();
+
+    if (error) {
+    return <div>Error: {error.message}</div>;
+}
+
+    const tables = ['iris', 'boston'];
+  
+    const tableQueries = await Promise.all(
+        tables.map(async (tableName) => {
+            const { data: tableData } = await supabase
+            .from(tableName as any)
+            .select('*')
+            .eq('dataset_id', id);
+      
+        return {
+            tableName,
+            data: tableData || [],
+            hasData: tableData && tableData.length > 0
+        } as { tableName: string; data: any[]; hasData: boolean };
+        })
+    );
+
+    const tablesWithData = tableQueries.filter(table => table.hasData);
+
+    const allData = {
+        table: tablesWithData.reduce((acc, table) => {
+            acc[table.tableName] = table.data;
+            return acc;
+        }, {} as Record<string, any[]>)
+    };
+
+    return (
+    <div className="p-4">
+        <h1 className="text-2xl font-bold">{data.title}</h1>
+            <pre>
+                {JSON.stringify(data, null, 2)}
+            </pre>
+
+            <pre>
+                {JSON.stringify(allData, null, 2)}
+            </pre>
+    </div>
+    );
+}
--- neurasect/src/app/page.tsx
+++ neurasect/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 interface Dataset {
   id: string;
@@ -19,11 +20,15 @@ export default function Home() {
     }, []);
 
   return (
-    <div>
-      <h1> Datasets:</h1>
-        <ul className="mt-4 space-y-2">
+    <div className="centered-div">
+      <h1 className="text-2xl">Datasets:</h1>
+        <ul className="list-disc mt-4 space-y-2">
             {datasets.map((dataset) => (
-            <li key={dataset.id}>{dataset.title}</li>
+            <li key={dataset.id}>
+              <Link href={`/datasets/${dataset.id}`}>
+              {dataset.title}
+              </Link>
+            </li>
           ))}
         </ul>
     </div>
