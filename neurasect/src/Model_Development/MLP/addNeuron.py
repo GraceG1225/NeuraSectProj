bb2eae574fb1acfed9ba5115eea85bd9c37acaa9
Param Desai <param-ridham.desai.167@my.csun.edu>
2025-10-11 22:10:22 -0700

    intilizaing new model when new layer is called and writing edit layer method

--- neurasect/src/Model_Development/MLP/addNeuron.py
+++ neurasect/src/Model_Development/MLP/addNeuron.py
@@ -32,8 +32,18 @@ class MLP:
         self.target_values = target_values
         self.num_of_layers = num_of_layers
         self.num_of_neurons_per_layer = np.array(num_of_neurons_per_layer)
-
-    def add_Neurons(self,nuerons=1,activation='relu',layer_number):    
+    
+    def add_layer(self,layer_number,neurons,activation='relu'):
+        self.num_of_neurons_per_layer[layer_number-1] = neurons
+        model.add(layers.Dense(neurons,activation)))
+    def edit_layer(self,layer_number,neurons,activation='relu'):
+        if layer_number !=1:
+            new_model = model.Sequential([
+                old_model.layers[:layer_number],
+                layers.Dense(neurons,activation),
+                old_model.layers[layer_number:]
+            ])    
+    def add_Neurons(self,nuerons=1,layer_number):    
         l_i = layer_number-1 # layer index
         total_nuerons = self.num_of_neurons_per_layer[l_i] + neurons
         editlayer(total_neurons)
